0630316849c51b34c4fa5e5941428a6230a66a13
ct-yipatenko <yaroslav.ipatenko@ctdev.io>
2019-04-05 14:41:13 +0300

    task 1

---
+++ Exception/src/com/company/Task1/Main.java
@@ -0,0 +1,15 @@
+package com.company.Task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number=2;
+	try{
+
+        System.out.println("Number < 0");
+    }
+	catch (Exception ex){
+	    number = 0;
+    }
+    }
+}
---
+++ Exception/src/com/company/Task1/MyException.java
@@ -0,0 +1,5 @@
+package com.company.Task1;
+
+public class MyException {
+
+}
